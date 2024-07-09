3fd97142a2aa04bba7b926f4491707be062ae2ec
OliJMatthews <olimatthews01@gmail.com>
2024-07-09 14:06:53 +0100

    Added more datasets

--- FirstShinyApp/app1.R
+++ FirstShinyApp/app1.R
@@ -6,6 +6,15 @@ UKBirths <- read.table("/Lei_Group_Shiny_App_Project/UKBirths.txt", header=TRUE,
 UKDeaths <- read.table("/Lei_Group_Shiny_App_Project/UKDeaths.txt", header=TRUE, quote="\"")
 AustriaBirths <- read.table("/Lei_Group_Shiny_App_Project/AustriaBirths.txt", header=TRUE, quote="\"")
 AustriaDeaths <- read.table("/Lei_Group_Shiny_App_Project/AustriaDeaths.txt", header=TRUE, quote="\"")
+DenmarkBirths <- read.table("/Lei_Group_Shiny_App_Project/DenmarkBirths.txt", header=TRUE, quote="\"")
+DenmarkDeaths <- read.table("/Lei_Group_Shiny_App_Project/DenmarkDeaths.txt", header=TRUE, quote="\"")
+IrelandBirths <- read.table("/Lei_Group_Shiny_App_Project/IrelandBirths.txt", header=TRUE, quote="\"")
+IrelandDeaths <- read.table("/Lei_Group_Shiny_App_Project/IrelandDeaths.txt", header=TRUE, quote="\"")
+NorwayBirths <- read.table("/Lei_Group_Shiny_App_Project/IrelandBirths.txt", header=TRUE, quote="\"")
+NorwayDeaths <- read.table("/Lei_Group_Shiny_App_Project/IrelandDeaths.txt", header=TRUE, quote="\"")
+SwitzerlandBirths <- read.table("/Lei_Group_Shiny_App_Project/IrelandBirths.txt", header=TRUE, quote="\"")
+SwitzerlandDeaths <- read.table("/Lei_Group_Shiny_App_Project/IrelandDeaths.txt", header=TRUE, quote="\"")
+
 
 # Adding Country, Pivoting Longer to add Sex Column and removing Total
 UKBirths <- UKBirths %>% 
@@ -26,8 +35,47 @@ AustriaDeaths <- AustriaDeaths %>%
   pivot_longer(c("Male","Female"),names_to="Sex") %>% 
   mutate(Total=NULL)
 
-Births <- bind_rows(AustriaBirths, UKBirths)
-Deaths <- bind_rows(AustriaDeaths,UKDeaths)
+DenmarkBirths <- DenmarkBirths %>% 
+  mutate(Country="Denmark") %>% 
+  pivot_longer(c("Male","Female"),names_to="Sex") %>% 
+  mutate(Total=NULL)
+DenmarkDeaths <- DenmarkDeaths %>% 
+  mutate(Country="Denmark") %>% 
+  pivot_longer(c("Male","Female"),names_to="Sex") %>% 
+  mutate(Total=NULL)
+
+IrelandBirths <- IrelandBirths %>% 
+  mutate(Country="Ireland") %>% 
+  pivot_longer(c("Male","Female"),names_to="Sex") %>% 
+  mutate(Total=NULL)
+IrelandDeaths <- IrelandDeaths %>% 
+  mutate(Country="Ireland") %>% 
+  pivot_longer(c("Male","Female"),names_to="Sex") %>% 
+  mutate(Total=NULL)
+
+NorwayBirths <- NorwayBirths %>% 
+  mutate(Country="Norway") %>% 
+  pivot_longer(c("Male","Female"),names_to="Sex") %>% 
+  mutate(Total=NULL)
+NorwayDeaths <- NorwayDeaths %>% 
+  mutate(Country="Norway") %>% 
+  pivot_longer(c("Male","Female"),names_to="Sex") %>% 
+  mutate(Total=NULL)
+
+SwitzerlandBirths <- SwitzerlandBirths %>% 
+  mutate(Country="Switzerland") %>% 
+  pivot_longer(c("Male","Female"),names_to="Sex") %>% 
+  mutate(Total=NULL)
+SwitzerlandDeaths <- SwitzerlandDeaths %>% 
+  mutate(Country="Switzerland") %>% 
+  pivot_longer(c("Male","Female"),names_to="Sex") %>% 
+  mutate(Total=NULL)
+
+
+
+
+Births <- bind_rows(AustriaBirths,UKBirths,DenmarkBirths,IrelandBirths,NorwayBirths,SwitzerlandBirths)
+Deaths <- bind_rows(AustriaDeaths,UKDeaths,DenmarkDeaths,IrelandDeaths,NorwayDeaths,SwitzerlandDeaths)
 
 
 ui <- fluidPage(
@@ -35,7 +83,7 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
           checkboxGroupInput("sex","Sex:",c("Male","Female")),
-          selectInput("country","Country (select 2):",c("UK","Austria"),multiple=TRUE)
+          selectInput("country","Country (select 2):",c("UK","Austria","Denmark","Ireland","Norway","Switzerland"),multiple=TRUE)
         ),
         mainPanel(
            column(6,textOutput("countryA"),plotOutput("chartB1"),
